e850390440145b99e99b7b1c4933cea131a1b363
monowar <monowar.hossain@gmail.com>
2019-09-26 01:41:57 -0500

    initial commit

--- lib/settings/settings.dart
+++ lib/settings/settings.dart
@@ -32,14 +32,26 @@ class Settings{
     Settings s = new Settings();
     s.motionsense_enable = json['motionsense_enable']??true;
     s.motionsense_debugEnable = json['motionsense_debugEnable']??true;
-    s.motionsense_requiredDeviceNo = json['motionsense_requiredDeviceNo']??0;
-    var list = (json['motionsense_devices']==null?List(): json['motionsense_devices'] as List);
-    if(list==null || list.length==0) s.motionsense_devices=List<DeviceSettings>();
+    if(json['motionsense_requiredDeviceNo']==null) s.motionsense_requiredDeviceNo=0;
+    else {
+      double x = json['motionsense_requiredDeviceNo'];
+      s.motionsense_requiredDeviceNo = x.round();
+    }
+
+    var list;
+    if(json['motionsense_devices']==null)
+      list = new List();
+    else list = json['motionsense_devices'] as List;
+    if(list==null || list.length==0) s.motionsense_devices=new List();
     else
     s.motionsense_devices = list.map((i) => DeviceSettings.fromJson(i)).toList();
-    list = (json['motionsense_defaultSettings']==null?List():json['motionsense_defaultSettings'] as List);
-    if(list==null || list.length==0) s.motionsense_defaultSettings=List<DeviceSettings>();
-    s.motionsense_defaultSettings = list.map((i) => DeviceSettings.fromJson(i)).toList();
+    var list1;
+    if(json['motionsense_defaultSettings']==null)
+      list1=new List();
+    else
+    list1 = json['motionsense_defaultSettings'] as List;
+    if(list1==null || list1.length==0) s.motionsense_defaultSettings=List();
+    s.motionsense_defaultSettings = list1.map((i) => DeviceSettings.fromJson(i)).toList();
     return s;
   }
 
